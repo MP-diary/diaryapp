b89290f7badae101938498123b09ec3fd62dd298
Catherine-Kim <woojong610@gmail.com>
2015-10-09 22:09:01 +0900

    DailyActivity 선언

---
+++ app/src/main/java/com/example/test01/DailyActivity.java
@@ -0,0 +1,15 @@
+package com.example.test01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 김우정 on 2015-10-09.
+ */
+public class DailyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily);
+    }
+}
